32ab852305c1c8dceac386be08f00c4479a342bb
Hein Andre Grønnestad <hag@haugnett.no>
2022-03-19 17:25:28 +0100

    Added episodes.h and hosts.h

---
+++ hosts.h
@@ -0,0 +1,11 @@
+char *hosts_name[] = {
+    "DAN WOOD",
+    "RAVI ABBOTT",
+    "JOE FOX"
+};
+
+char *hosts_bio[] = {
+    "Dan has been a fan of video games and computers for as long as he can remember. He is an avid hardware collector, currently estimating his collection at over 80 sysems (he has a very understanding wife). Dan's background is in broadcasting, audio production and stage hosting, skills which he brings to the podcast each week.\n\nDan is a prominent Youtuber, and can be found telling tech-tales and exploring his love of leftfield and vintage technology.",
+    "Ravi is a self-confessed 'nerd' and retro enthusiast with deep connections and history within the UK's retro computer and digital arts scene. Growing up, Ravi was involved in organising some of the first retro video gaming events and festivals in the UK.\n\nRavi's connections mean we are able to bring you the biggest names in the industry to the show each week. Ravi also provides detailed research for each guests, aiming to keep the podcast entertaining but factual.",
+    "Joe brings the 'rock n roll' to the Retro Hour Podcast. Outside of his job as a lead vocalist in a metal band, he is also a life-long retro gaming fan, particularly of Sega and Nintendo consoles. Joe's wealth of console system and game knowledge means the podcast is in touch with the history and future of these systems.\n\n\"Handsome Joe\", as he is affectionately nicknamed by podcast listeners, is also the lead vocalist of A Hundred Crowns."
+};
